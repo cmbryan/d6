24d6c0b3ec9c2e6342107e91633d74d6174dc43e
Chris Bryan <chris.bryan2@nhs.net>
2025-03-22 22:36:19 +0000

    Fixing API documentation

--- d6-api/d6_api/app.py
+++ d6-api/d6_api/app.py
@@ -1,11 +1,30 @@
 from flask import current_app, request, jsonify
-from flask_restx import Api, Resource
+from flask_restx import Api, Resource, fields
 
 from . import models
 from .app_logic import simulate_attack
 
 
 api = Api()
+
+api_unit = api.model('Unit', {
+    'id': fields.Integer,
+    'name': fields.String,
+    'toughness': fields.Integer,
+    'save': fields.String,
+    'weapons': fields.List(fields.Integer),
+})
+
+api_weapon = api.model('Weapon', {
+    'id': fields.Integer,
+    'name': fields.String,
+    'weapon_skill': fields.String,
+    'strength': fields.Integer,
+    'attacks': fields.Integer,
+    'armour_penetration': fields.Integer,
+    'units': fields.List(fields.Integer),
+})
+
 
 @api.route("/attack")
 @api.doc(params={'id': 'An ID'})
@@ -24,8 +43,10 @@ class Attack(Resource):
         return jsonify(result)
 
 
+@api.route("/units")
 class ListUnits(Resource):
+
+    @api.marshal_with(api_unit, as_list=True)
     def get(self):
         with current_app.app_context():
-            units = [unit.to_table_dict() for unit in models.Unit.query.all()]
-        return jsonify(units)
+            return [unit.to_table_dict() for unit in models.Unit.query.all()]
